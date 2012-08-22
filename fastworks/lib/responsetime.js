ef20cc3e3d0124eaae682b34b8fbf54d23b8bac6
RobeeeJay <robert@reviewer.co.uk>
2012-08-22 16:05:07 +0000

    Finally fixed ResponseTime, I hope

--- fastworks/lib/responsetime.js
+++ fastworks/lib/responsetime.js
@@ -9,27 +9,32 @@ DESCRIPTION
 *******************************************************************************************/
 
 var fw = require("./framework");
+var http = require("http");
 
 // Simple response time header
 function ResponseTime(args)
 {
 	this.Stack(args);
+	
+	// I don't really like doing it this way, but seems the best way to hook the headers
+	if (!http.ServerResponse.prototype._fw_oldwriteHead)
+	{
+		http.ServerResponse.prototype._fw_oldwriteHead = http.ServerResponse.prototype.writeHead;
+		
+		http.ServerResponse.prototype.writeHead = function() {
+			if (this._fw_starttime)
+				this.setHeader("X-Response-time", (new Date() - this._fw_starttime) + "ms");
+	  		
+	  		return http.ServerResponse.prototype._fw_oldwriteHead.apply(this, arguments);
+	    };
+	}
 }
 
 fw.Inherits(ResponseTime, fw.Stack);
 
 ResponseTime.prototype.processStack = function(request, response, destination)
 {
-	var dtStart = new Date;
-    var oldRenderHeaders = response._renderHeaders;
-
-	// I don't really like doing it this way, but seems the best way to hook the headers
-	response._renderHeaders = function() {
-		response.setHeader("X-Response-time", (new Date() - dtStart) + "ms");
-  		
-  		return oldRenderHeaders.apply(response, arguments);
-    };
-    
+	response._fw_starttime = new Date;
 	
 	this.next.processStack(request, response, destination);
 }
